90c04c6d0225932dbc630dc039a57975d6418912
Tobias Matthies <tbsmatthies@gmail.com>
2022-05-12 17:04:54 -0400

    drawn first characters, tried own moving function

---
+++ models/character.class.js
@@ -0,0 +1,13 @@
+class Character extends MovableObject {
+ 
+
+    constructor() {
+        super().loadImage('img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-21.png');
+    }
+
+
+
+    jump() {
+
+    }
+}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,10 +1,23 @@
 class MovableObject {
-    x;
-    y;
+    x = 120;
+    y = 400;
     img;
+    width = 200;
+    height = 300;
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
 
 
     moveRight() {
         console.log('Moving right')
+    }
+
+
+    moveLeft() {
+        
     }
 }
